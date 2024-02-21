b329dce247f0edf14501d9b7164d680737ad51a2
RahulProjects01 <srahulb6@gmail.com>
2024-02-21 18:28:46 +0530

    first project

---
+++ learnspringbatch-2/learnspringbatch-2/src/main/java/com/learnspringbatch2/learnspringbatch2/Learnspringbatch2Application.java
@@ -0,0 +1,13 @@
+package com.learnspringbatch2.learnspringbatch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learnspringbatch2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learnspringbatch2Application.class, args);
+	}
+
+}
---
+++ learnspringbatch-2/learnspringbatch-2/src/test/java/com/learnspringbatch2/learnspringbatch2/Learnspringbatch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringbatch2.learnspringbatch2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learnspringbatch2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
